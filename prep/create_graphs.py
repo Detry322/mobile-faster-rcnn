52925396073f26fcd2aae10454e25ec4f5afac51
Jack Serrino <Jserrino@gmail.com>
2018-03-15 11:56:29 +0800

    Fix net

--- prep/create_graphs.py
+++ prep/create_graphs.py
@@ -25,56 +25,28 @@ from faster_rcnn.nets.mobilenet_v1 import mobilenetv1
 def canonical_name(x):
     return x.name.split(":")[0]
 
-# def create_graph(sess, net, model_checkpoint_filename, output_dir):
-#     _, name = os.path.split(model_checkpoint_filename)
-#     base_output_name = os.path.join(output_dir, name).rstrip('.ckpt') + '-complete'
-#     names = net.create_architecture(mode='TEST', tag='default', num_classes=81, anchor_scales=(8, 16, 32, 64))
-#     for i in sess.graph_def.node:
-#         print("BEFORE: " + i.name)
-
-#     saver = tf.train.Saver()
-#     print("Restoring initial graph...")
-#     saver.restore(sess, model_checkpoint_filename)
-#     for i in sess.graph_def.node:
-#         print(" AFTER: " +i.name)
-
-
-#     out_tensors = sorted(names.values(), key=lambda n: n.name)
-#     print("Freezing graph...")
-#     frozen_graphdef = tf.graph_util.convert_variables_to_constants(
-#         sess, sess.graph_def, map(canonical_name, out_tensors))
-
-#     for i in frozen_graphdef.node:
-#     	print("FROZEN: " + i.name)
-
-
-#     frozen_graphdef_out = base_output_name + '.pb'
-#     print("Writing frozen graph...")
-#     with open(frozen_graphdef_out, 'w') as f:
-#         f.write(frozen_graphdef.SerializeToString())
-#     for i, (k, v) in enumerate(names.items()):
-#         print(" ==== Output {}: {}, {}".format(i, k, v.name))
-
-import cv2
-import numpy as np
-
-def load_image(image_name):
-    im = cv2.imread(image_name)
-    fixed_im = im - np.array([[[102.9801, 115.9465, 122.7717]]])
-    return fixed_im.reshape((1,) + fixed_im.shape)
-
-def create_graph(image, sess, net, model_checkpoint_filename, output_dir):
-    img = load_image(image)
+def create_graph(sess, net, model_checkpoint_filename, output_dir):
     _, name = os.path.split(model_checkpoint_filename)
     base_output_name = os.path.join(output_dir, name).rstrip('.ckpt') + '-complete'
     names = net.create_architecture(mode='TEST', tag='default', num_classes=81, anchor_scales=(8, 16, 32, 64))
+
     saver = tf.train.Saver()
     print("Restoring initial graph...")
     saver.restore(sess, model_checkpoint_filename)
 
-    cls_score, cls_prob, bbox_pred, rois = net.test_image(sess, img, img.shape[1:])
+    out_tensors = sorted(names.values(), key=lambda n: n.name)
+    print("Freezing graph...")
+    frozen_graphdef = tf.graph_util.convert_variables_to_constants(
+        sess, sess.graph_def, map(canonical_name, out_tensors))
 
-    import pdb; pdb.set_trace()
+    frozen_graphdef_out = base_output_name + '.pb'
+    print("Writing frozen graph...")
+    with open(frozen_graphdef_out, 'w') as f:
+        f.write(frozen_graphdef.SerializeToString())
+    for i, v in enumerate([net._image, net._im_info]):
+        print(" ===== Input {}: {}".format(i, v.name)) 
+    for i, (k, v) in enumerate(names.items()):
+        print(" ==== Output {}: {}, {}".format(i, k, v.name))
 
 
 def parse_args():
@@ -98,7 +70,7 @@ def get_net(net_name):
 def run_func(args, func):
     with tf.Session() as sess:
         net = get_net(args.net)
-        func(args.image, sess, net, args.checkpoint, args.output)
+        func(sess, net, args.checkpoint, args.output)
 
 def main():
     args = parse_args()
--- prep/faster_rcnn/layer_utils/proposal_layer.py
+++ prep/faster_rcnn/layer_utils/proposal_layer.py
@@ -67,9 +67,9 @@ def proposal_layer_tf(rpn_cls_prob, rpn_bbox_pred, im_info, cfg_key, _feat_strid
   proposals = bbox_transform_inv_tf(anchors, rpn_bbox_pred)
   proposals = clip_boxes_tf(proposals, im_info[:2])
 
-  indices = tf.image.non_max_suppression(rpn_bbox_pred, scores, max_output_size=post_nms_topN, iou_threshold=nms_thresh)
+  indices = tf.image.non_max_suppression(proposals, scores, max_output_size=post_nms_topN, iou_threshold=nms_thresh)
 
-  boxes = tf.gather(rpn_bbox_pred, indices)
+  boxes = tf.gather(proposals, indices)
   boxes = tf.to_float(boxes)
   scores = tf.gather(scores, indices)
   scores = tf.reshape(scores, shape=(-1, 1))
